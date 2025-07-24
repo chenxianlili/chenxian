49f12229b5aa6cdefb1ff09d0d52c612c54c8df3
chenxianlili <chenxiancx@foxmail.com>
2025-07-24 23:45:47 +0800

    Update test1.java

--- baohe/src/test/java/test1.java
+++ baohe/src/test/java/test1.java
@@ -2,11 +2,19 @@ import org.testng.annotations.Test;
 
 public class test1 {
     @Test
-    public void test133(){
-        System.out.println("test——success 133");
+    public void case1(){
+        System.out.println("test——success 23424");
         int[] array = {1,2,3,4,5};
         for (int i = 0; i < array.length; i++) {
            System.out.println(array[i]); 
         }
     }
-}
+      @Test(description = "123")
+    public void case2(){
+        System.out.println("test——success 3243423423423");
+        int[] array = {1,2,3,4,5};
+        for (int i = 0; i < array.length; i++) {
+           System.out.println(array[i]); 
+        }
+    }
+}   
