e5d5bd7cffecb7374db2fcf4340c8aa71d4820e2
chenxian <1014896442@qq.com>
2025-07-24 23:18:46 +0800

    123123

---
+++ baohe/src/test/java/test1.java
@@ -0,0 +1,12 @@
+import org.testng.annotations.Test;
+
+public class test1 {
+    @Test
+    public void test133(){
+        System.out.println("test——success 133");
+        int[] array = {1,2,3,4,5};
+        for (int i = 0; i < array.length; i++) {
+           System.out.println(array[i]); 
+        }
+    }
+}
