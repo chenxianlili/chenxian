702b3791fbe2bfffc87cce3f480b9548b20be5c0
chenxianlili <chenxiancx@foxmail.com>
2025-07-24 23:45:47 +0800

    Update test1.java

--- baohe/src/test/java/test1.java
+++ baohe/src/test/java/test1.java
@@ -2,7 +2,7 @@ import org.testng.annotations.Test;
 
 public class test1 {
     @Test
-    public void test133(){
+    public void test1(){
         System.out.println("test——success 133");
         int[] array = {1,2,3,4,5};
         for (int i = 0; i < array.length; i++) {
